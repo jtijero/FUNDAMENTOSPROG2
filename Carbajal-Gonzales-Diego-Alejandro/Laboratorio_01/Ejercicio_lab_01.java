eb7aea97a0e0e4d0293f6631fd0f3be84e86d05f
Diego <138242587+L0rD1ego@users.noreply.github.com>
2023-09-22 21:16:46 -0500

    Añadir Laboratorio01

---
+++ Carbajal-Gonzales-Diego-Alejandro/Laboratorio_01/Ejercicio_lab_01.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Ejercicio_lab_01{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = sc.nextLine();
+        String d = sc.nextLine();
+        String e = sc.nextLine();
+        System.out.println("Los nombres de los soldados son:"+a+","+b+","+c+","+d+","+e );
+    }
+}
---
+++ Carbajal-Gonzales-Diego-Alejandro/Laboratorio_01/Ejercicio_lab_02.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Ejercicio_lab_02{
+    public static void main(String[] args){
+        System.out.println("ingrese el nombre y la salud");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String va = sc.nextLine();
+        String b = sc.nextLine();
+        String vb = sc.nextLine();
+        String c = sc.nextLine();
+        String vc = sc.nextLine();
+        String d = sc.nextLine();
+        String vd = sc.nextLine();
+        String e = sc.nextLine();
+        String ve = sc.nextLine();
+        System.out.println("Los nombres de los soldados son:"+a+","+b+","+c+","+d+","+e );
+        System.out.println("su vida es:"+va+","+vb+","+vc+","+vd+","+ve );
+    }    
+}
