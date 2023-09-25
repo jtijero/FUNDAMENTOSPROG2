1ddccf797d2c9280ced13addaf8ed11c59ba4e10
MMaraP <dmara@unsa.edu.pe>
2023-09-25 06:33:11 -0500

    Avance laboratorio2

--- MARA-MAMANI-DONNY-MOISES/Laboratorio_02/Ahorcado.java
+++ MARA-MAMANI-DONNY-MOISES/Laboratorio_02/Ahorcado.java
@@ -36,7 +36,7 @@ public class Ahorcado{
 	String ahor5 = " +---+ \n" +
 				   " |   | \n" +
 				   " 0   | \n" +
-				   "/|\\ | \n" +
+				   "/|\\| \n" +
 				   "     | \n" +
 				   "     | \n" +
 				   "=========";
@@ -44,7 +44,7 @@ public class Ahorcado{
 	String ahor6 = " +---+ \n" +
 				   " |   | \n" +
 				   " 0   | \n" +
-				   "/|\\ | \n" +
+				   "/|\\| \n" +
 				   "/    | \n" +
 				   "     | \n" +
 				   "=========";
@@ -52,25 +52,28 @@ public class Ahorcado{
 	String ahor7 = " +---+ \n" +
 				   " |   | \n" +
 				   " 0   | \n" +
-				   "/|\\ | \n" +
-				   "/ \\ | \n" +
+				   "/|\\| \n" +
+				   "/ \\| \n" +
 				   "     | \n" +
 				   "=========";
     String[] figuras = {ahor1, ahor2, ahor3, ahor4, ahor5, ahor6, ahor7};
-    int contador = 0;
+    int contador = 1;
     String letra;
     String[] palabras = {"programacion", "java", "identacion", "clases", "objetos", "desarrollador", "pruebas"};
     String palSecreta = getPalabraSecreta(palabras);
     System.out.println(figuras[0]);
-    mostrarBlancos(palSecreta);
+    String[] blanco = blanco(palSecreta);
+	mostrarBlancos(palSecreta);
     System.out.println("\n");
     while(contador <= 6){
     	letra = ingreseLetra();
     	if (letraEnPalabraSecreta(letra, palSecreta)){
+			blanco = blancoActualizado(blanco, palSecreta, letra);
 			System.out.println(figuras[contador]);
-    		mostrarBlancosActualizados(letra);
+    		mostrarBlancosActualizados(blanco);
     	}else{
     	  System.out.println(figuras[contador]);
+		  mostrarBlancosActualizados(blanco);
     	  contador = contador +1;
     	}
     	//COMPLETAR PARA INDICAR SI GANÓ, PERDIÓ Y CUANTOS TURNOS NECESITÓ
@@ -93,7 +96,7 @@ public class Ahorcado{
   public static String ingreseLetra() {
     String laLetra;
     Scanner sc = new Scanner(System.in);
-	System.out.println("\n");
+	System.out.println();
     System.out.println("Ingrese letra: ");
     laLetra = sc.next();
     while(laLetra.length() != 1) {
@@ -112,8 +115,31 @@ public class Ahorcado{
 	}
 	return false;
   }
-  public static void mostrarBlancosActualizados(String letra) {
+
+  public static String[] blanco(String palSecreta){
+	String[] progreso = new String[palSecreta.length()];
+	for(int i = 0; i < progreso.length; i++){
+	  progreso[i] = "_";
+	}
+	return progreso;
+  }
+
+  public static String[] blancoActualizado(String[] progreso, String palSecreta, String letra){
+	String[] progresoActualizado = new String[progreso.length];
+	for(int i = 0; i < palSecreta.length(); i++){
+	  if(letra.equals(palSecreta.substring(i, i + 1))){
+		progresoActualizado[i] = letra;
+	  }else{
+		progresoActualizado[i] = progreso[i];
+	  }
+	}
+	return progresoActualizado;
+  }
+
+  public static void mostrarBlancosActualizados(String[] progreso) {
 	//COMPLETAR
-	
+	for(int i = 0; i < progreso.length; i++){
+	  System.out.print(" " + progreso[i]);
+	}
   }
 }
